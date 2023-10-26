daf1fe3aeec4ef2fb1e7682ca4d0c8d428fd40f8
1NxT <guirodrigues7863@gmail.com>
2023-10-26 17:36:05 -0300

    Impl list push

--- src/first.rs
+++ src/first.rs
@@ -1,11 +1,14 @@
+use std::mem;
+
 pub struct List {
     head: Link,
 }
 
 struct Node {
     elem: i32,
-    next: List,
+    next: Link,
 }
+
 enum Link {
     Empty,
     More(Box<Node>),
@@ -14,5 +17,14 @@ enum Link {
 impl List {
     pub fn new() -> Self {
         List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, element: i32) {
+        let new_node = Box::new(Node {
+            elem: element,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
     }
 }
